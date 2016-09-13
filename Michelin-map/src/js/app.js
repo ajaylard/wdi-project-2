4d443668a1cd27a85b7a6c0c898f1f620f6d4842
ajaylard <ajay.lard@gmail.com>
2016-09-13 11:59:37 +0100

    just got authentication working ont he front end

--- Michelin-map/src/js/app.js
+++ Michelin-map/src/js/app.js
@@ -170,7 +170,7 @@ App.register = function() {
       <input type="password" name="user[passwordConfirmation]">
       <br>
       <br>
-      <input type="submit" value="Signup">
+      <input type="submit" value="Sign up">
     </form>
     `);
 };
@@ -204,7 +204,6 @@ App.ajaxRequest = function(url, method, data, callback){
   });
 };
 
-
 App.logout = function() {
   event.preventDefault();
   this.removeToken();
@@ -214,8 +213,6 @@ App.logout = function() {
 App.loggedInState = function(){
   $(".loggedOut").hide();
   $(".loggedIn").show();
-
-  
 };
 
 App.loggedOutState = function(){
@@ -237,6 +234,18 @@ App.getToken = function(){
 
 App.removeToken = function(){
   return window.localStorage.clear();
+};
+
+App.mapSetup = function() {
+  let canvas = document.getElementById('map-canvas');
+  let mapOptions = {
+    zoom: 13,
+    center: new google.maps.LatLng(51.506178,-0.088369),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#0066ff"},{"saturation":74},{"lightness":100}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"},{"weight":0.6},{"saturation":-85},{"lightness":61}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#5f94ff"},{"lightness":26},{"gamma":5.86}]}]
+  };
+
+  this.map = new google.maps.Map(canvas, mapOptions);
 };
 
 $(App.init.bind(App));
